64f9bb728d88bd3a2cba394955bc588c06487d39
raniwhydonate <rani@whydonate.com>
2023-10-19 16:07:59 +0530

    section2 completed

--- Section2/my-first-app/src/app/servers/servers.component.ts
+++ Section2/my-first-app/src/app/servers/servers.component.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: ` <app-server></app-server>
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css'],
+  // selector: '[app-servers]',  //select by attribute
+  selector: '.app-servers', //select by class
+  // template: ` <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  //styleUrls: ['./servers.component.css'],
+  styles:[`
+  .online{
+    color:white;
+  }`]
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    //console.log(event);
+  }
+}
