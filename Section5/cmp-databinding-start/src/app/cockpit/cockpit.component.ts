1a6fa6ad5b690e3598696d9cfd6a7464ffe91424
raniwhydonate <rani@whydonate.com>
2023-10-23 14:03:28 +0530

    Binding to custom events

--- Section5/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ Section5/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,34 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent implements OnInit {
-
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output() blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
   newServerName = '';
   newServerContent = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+   this.serverCreated.emit({
+    serverName:this.newServerName,
+    serverContent:this.newServerContent
+   })
   }
 
   onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.blueprintCreated.emit({
+      serverName:this.newServerName,
+      serverContent:this.newServerContent
+     })
   }
-  
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
