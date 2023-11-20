d381eb82889286dd07bffb7cdffb5bc0c71725d4
raniwhydonate <rani@whydonate.com>
2023-11-20 13:34:41 +0530

    routing section

--- Section11/routing-final/src/app/app.module.ts
+++ Section11/routing-final/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,14 +11,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UserComponent },
-  { path: 'users/:id/:name', component: UserComponent },
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent },
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 @NgModule({
   declarations: [
@@ -29,9 +28,15 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [ServersService],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- Section11/routing-final/src/app/users/user/user.component.ts
+++ Section11/routing-final/src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -12,24 +12,23 @@ export class UserComponent implements OnInit, OnDestroy {
   paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
-  
+
   ngOnInit() {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    
     this.paramsSubscription = this.route.params
-    .subscribe(
-      (params: Params)=>{
-        this.user.id=params['id'];
-        this.user.name=params['name'];
-      }
-      )
-    }
-    
-      ngOnDestroy(): void {
-        this.paramsSubscription.unsubscribe();
-      }
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
 
 }
